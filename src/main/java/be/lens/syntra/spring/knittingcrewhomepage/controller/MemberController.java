e1d0896a8119fc0df9363f4cc992ed8fbb77e43d
lens <lens.huygh@gmail.com>
2020-02-15 20:40:40 +0100

    add method for testing edit controller for testing another form of the edit page add formatting of date in model with annotation add edittest.html to test other way of using the form with existing object change building href in overview

--- src/main/java/be/lens/syntra/spring/knittingcrewhomepage/controller/MemberController.java
+++ src/main/java/be/lens/syntra/spring/knittingcrewhomepage/controller/MemberController.java
@@ -1,10 +1,12 @@
 package be.lens.syntra.spring.knittingcrewhomepage.controller;
 
 import be.lens.syntra.spring.knittingcrewhomepage.model.KnittingCrewRole;
+import be.lens.syntra.spring.knittingcrewhomepage.model.Member;
 import be.lens.syntra.spring.knittingcrewhomepage.model.Stitches;
 import be.lens.syntra.spring.knittingcrewhomepage.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,19 +26,24 @@ public class MemberController {
         return new ModelAndView("overview", "members", memberService.getAllMembers());
     }
 
-    @GetMapping("/memberDetail{id}")
+    @GetMapping("/memberDetail/{id}")
     public ModelAndView displayDetail(@PathVariable String id){
-        memberService.getMemberById(Integer.parseInt(id));
+        //memberService.getMemberById(Integer.parseInt(id));
         return new ModelAndView("detail", "member",memberService.getMemberById(Integer.parseInt(id)));
     }
 
     @GetMapping("/editMember{id}")
     public ModelAndView displayEditingMember(@PathVariable String id){
-        memberService.getMemberById(Integer.parseInt(id));
+        //memberService.getMemberById(Integer.parseInt(id));
         Map<String,Object> modelMap = new HashMap<>();
         modelMap.put("member",memberService.getMemberById(Integer.parseInt(id)));
         modelMap.put("roles", KnittingCrewRole.values());
         modelMap.put("stitches", Stitches.values());
         return new ModelAndView("edit", "modelMap",modelMap);
+    }
+
+    @GetMapping("/editTest")
+    public ModelAndView editTest(Model model){
+        return new ModelAndView("edittest","member",memberService.getMemberById(3));
     }
 }
--- src/main/java/be/lens/syntra/spring/knittingcrewhomepage/model/Member.java
+++ src/main/java/be/lens/syntra/spring/knittingcrewhomepage/model/Member.java
@@ -1,6 +1,7 @@
 package be.lens.syntra.spring.knittingcrewhomepage.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -18,6 +19,7 @@ public class Member {
     private KnittingCrewRole knittingCrewRole;
     private Address address;
     private String phone;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate birthDate;
     private Collection<Stitches> knownStitches;
 }
